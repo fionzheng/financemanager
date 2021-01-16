39182e1571dc63b6abe3401d126abf3adbc06d10
Neysa Patel <68453522+neysapatel@users.noreply.github.com>
2021-01-16 12:33:34 -0500

    Add files via upload

--- database/finance.sql
+++ database/finance.sql
@@ -1,1 +1,9 @@
+CREATE DATABASE databasename;
 
+CREATE TABLE Spending (
+    BankAcc int,
+    Groceries varchar(255),
+    Clothing varchar(255),
+    Entertainment varchar(255),
+    Furniture varchar(255)
+);
