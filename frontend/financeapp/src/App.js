268f056330f0b7d260f809f1e628c895a8520861
Ryan Kim <rdhkim0@gmail.com>
2021-01-17 03:25:56 -0500

    commit

--- frontend/financeapp/src/App.js
+++ frontend/financeapp/src/App.js
@@ -2,13 +2,16 @@ import {DropDown} from "./components_1/categories/categories.js"
 import {SimpleKeyEvent} from "./components_1/SimpleKeyEvent/SimpleKeyEvent.js"
 const NewComponent = DropDown;
 const NewComponent1 = SimpleKeyEvent;
+
 function App(){
   return(
     <div>
+      <h1 style={{backgroundColor: "lightblue"}}> 
       <NewComponent name ="DropDown">
       </NewComponent>
       <NewComponent1 name ="SimpleKeyEvent">
       </NewComponent1>
+      </h1>
     </div> 
   )
 }
